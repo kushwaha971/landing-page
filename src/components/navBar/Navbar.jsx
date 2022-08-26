6aa0f18f0ebe4811e7c46e7b46cbd662d99abaab
Akash <kushwahaakash971gmail.com>
2022-08-26 19:28:36 +0530

    smooth scroll

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -21,31 +21,54 @@ const Navbar = () => {
           <h1 className="nav_logo">Digital Agency</h1>
           <ul className={click ? "nav_menu_active" : "nav_menu"}>
             <li className="nav_items">
-              <Link exact to="home" className="nav_links ">
+              <Link
+                exact
+                spy={true}
+                smooth={true}
+                to="home"
+                className="nav_links "
+              >
                 Home
               </Link>
             </li>
             <li className="nav_items">
-              <Link exact to="about" className="nav_links">
+              <Link
+                exact
+                spy={true}
+                smooth={true}
+                to="about"
+                className="nav_links"
+              >
                 About
               </Link>
             </li>
             <li className="nav_items">
-              <Link exact to="testimonials" className="nav_links">
+              <Link
+                exact
+                spy={true}
+                smooth={true}
+                to="testimonials"
+                className="nav_links"
+              >
                 Testimonials
               </Link>
             </li>
             <li className="nav_items">
-              <Link exact to="contact" className="nav_links">
+              <Link
+                exact
+                spy={true}
+                smooth={true}
+                to="contact"
+                className="nav_links"
+              >
                 Contact
-              </Link> 
+              </Link>
             </li>
           </ul>
 
           <div className="nav_icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
-
         </div>
       </nav>
       <section id="home">{<Home />}</section>
